05eb9a5afc08ad28e99d4daa0a007ee535e88b20
Yasas Kakarla <yk2069969@02HW056289.lan>
2022-03-21 18:30:32 -0500

    Changing the package version for React Dom

--- skylight/src/App.js
+++ skylight/src/App.js
@@ -1,12 +1,12 @@
 import  { React } from 'react';
-import Main from "./components/MainComponent"
-import {BrowserRouter} from "react-router-dom"
+import Main from "./components/MainComponent";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Main/>
+        <Main/>
       </BrowserRouter>
     </div>
   );
