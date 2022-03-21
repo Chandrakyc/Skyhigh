d70487cccbfc8d55fa3c9a50da24c89e04d412b0
Yasas Kakarla <yk2069969@02HW056289.lan>
2022-03-21 18:57:30 -0500

    Redirecting

--- skylight/src/components/AboutComponent.js
+++ skylight/src/components/AboutComponent.js
@@ -1,8 +1,12 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function About() {
     return(
+        <React.Fragment>
         <div> About </div>
+        <Link to= "/home"> Click here for Home</Link>
+        </React.Fragment>
     )
 };
 
--- skylight/src/components/HomeComponent.js
+++ skylight/src/components/HomeComponent.js
@@ -1,8 +1,12 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function Home() {
     return(
+        <React.Fragment>
         <div> Home </div>
+        <Link to= "/about"> Click here for about</Link>
+        </React.Fragment>
     )
 };
 
